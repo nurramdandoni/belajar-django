621b8a72ad942aab1920703cdcb8b75e6581174f
Doni Nurramdan <doni@weplus.id>
2025-05-19 17:44:08 +0700

    [passing data] : sample passing data

--- about/models.py
+++ about/models.py
@@ -2,10 +2,15 @@ from django.db import models
 
 # Create your models here.
 
-class about(models.Model):
+class About(models.Model):
     preface = models.CharField(max_length=100)
     detail = models.CharField(max_length=255,null=True)
     created_by = models.IntegerField(max_length=11,default='',null=True)
     created_at = models.DateTimeField(default='',null=True)
     updated_by = models.IntegerField(max_length=11,default='',null=True)
     updated_at = models.DateTimeField(default='',null=True)
+    
+    @classmethod
+    def get_all_data(self):
+        data = self.objects.all().values()
+        return data
--- about/views.py
+++ about/views.py
@@ -2,10 +2,18 @@
 from django.template import loader
 
 from django.http import HttpResponse
+from .models import About
 
 # def about(request):
 #     return HttpResponse("Hello world!")
 
 def about(request):
     template = loader.get_template('about.html')
-    return HttpResponse(template.render())
+    dataAbout = About.objects.values()
+    context = {
+        "about" :dataAbout
+    }
+    
+    print(context)
+    
+    return HttpResponse(template.render(context,request))
