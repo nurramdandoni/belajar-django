0b7fbefaa12440988351ee22cc24a184a597822b
Doni Nurramdan <doni@weplus.id>
2025-05-19 14:59:34 +0700

    [migration] : sampling migration data

--- about/models.py
+++ about/models.py
@@ -4,8 +4,8 @@ from django.db import models
 
 class about(models.Model):
     preface = models.CharField(max_length=100)
-    detail = models.CharField(max_length=255)
-    created_by = models.IntegerField(max_length=11)
-    created_at = models.DateTimeField()
-    updated_by = models.IntegerField(max_length=11)
-    updated_at = models.DateTimeField()
+    detail = models.CharField(max_length=255,null=True)
+    created_by = models.IntegerField(max_length=11,default='',null=True)
+    created_at = models.DateTimeField(default='',null=True)
+    updated_by = models.IntegerField(max_length=11,default='',null=True)
+    updated_at = models.DateTimeField(default='',null=True)
