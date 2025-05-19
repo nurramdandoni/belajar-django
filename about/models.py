1cd8be626de83e56546ad6870aa6c8b77e0ec46f
Doni Nurramdan <doni@weplus.id>
2025-05-19 14:46:00 +0700

    [model] : add model about manualy

--- about/models.py
+++ about/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class about(models.Model):
+    preface = models.CharField(max_length=100)
+    detail = models.CharField(max_length=255)
+    created_by = models.IntegerField(max_length=11)
+    created_at = models.DateTimeField()
+    updated_by = models.IntegerField(max_length=11)
+    updated_at = models.DateTimeField()
